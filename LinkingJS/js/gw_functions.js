08adf54d61fd654a82b01922ed6ddea614ce4ca0
Matt Orme <imthe1whoknox@gmail.com>
2015-05-22 22:38:42 -0700

    more gw work

---
+++ LinkingJS/js/gw_functions.js
@@ -0,0 +1,36 @@
+/*
+var x;
+
+function myfunction(p1, p2) {
+	return p1 * p2;
+}
+
+
+x = parseInt(prompt("first number"));
+y = parseInt(prompt("second number"));
+
+z = myfunction(x, y);
+
+
+function capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+x = "mississippi";
+
+capitalize(x);
+
+*/
+
+
+function add(a, b, c, d, e) {
+   return (a + b + c + d + e);
+    }
+
+num1 = parseInt(prompt("pick a number:"));
+num2 = parseInt(prompt("pick a number:"));
+num3 = parseInt(prompt("pick a number:"));
+num4 = parseInt(prompt("pick a number:"));
+num5 = parseInt(prompt("pick a number:"));
+
+alert(add(num1, num2, num3, num4, num5));
